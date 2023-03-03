52a67aa5a3b3561af4b11c0dd502a6431cf8ba1a
c0b4lt-student <student.corentin.turgis@gmail.com>
2023-03-03 13:32:08 +0100

    modifiy index template for blogApiController, BlogApiController is comment

--- src/Controller/BlogApiController.php
+++ src/Controller/BlogApiController.php
@@ -8,7 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogApiController extends AbstractController
 {
-    #[Route('api/posts/{id}', methods: ['GET', 'HEAD'])]
+    //Here we can see two methods (f) "show and edit" who have same route.
+    //Show is called if url method is GET or HEAD
+    //edit is used if url method is POST
+    /*
+     * HTML forms only support GET and POST methods. If you're calling a route with a different method from an HTML form,
+     *  add a hidden field called _method with the method to use (e.g. <input type="hidden" name="_method" value="PUT">).
+     *  If you create your forms with Symfony Forms this is done automatically for you when the framework.http_method_override option is true.
+     */
+    #[Route('/posts/{id}', methods: ['GET', 'HEAD'])]
     public function show(int $id): Response
     {
         return $this->render('blog_api/index.html.twig', [
@@ -16,7 +24,7 @@ class BlogApiController extends AbstractController
         ]);
     }
 
-    #[Route('/api/posts/{id}', methods: ['PUTS'])]
+    #[Route('/posts/{id}', methods: ['PUTS'])]
     public function edit(int $id): Response
     {
         return $this->render('blog_api/index.html.twig', [
